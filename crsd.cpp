4c36d73c2b9cc0bda4030ebad096cc74c62fe053
Anh Nguyen <ng.huyanh.gd@gmail.com>
2023-02-08 21:24:37 -0600

    finish create and list

--- crsd.cpp
+++ crsd.cpp
@@ -20,9 +20,76 @@
 struct room
 {
     int port;
+    int master_socket;
     int max_socket;
     std::vector<int> slave_socket;
 };
+
+void debug(std::map<std::string, room> &database)
+{
+    for (std::map<std::string, room>::iterator iter = database.begin(); iter != database.end(); ++iter)
+    {
+        LOG(WARNING) << "Chatroom name: " << iter->first << "; port: " << iter->second.port << "; master socket: " << iter->second.master_socket;
+        if (iter->second.slave_socket.size())
+        {
+            LOG(WARNING) << "No socket"
+                         << "\n";
+        }
+        for (int i = 0; i < iter->second.slave_socket.size(); i++)
+        {
+            LOG(WARNING) << "socket: " << i;
+        }
+    }
+    LOG(WARNING) << "\n";
+}
+
+int new_socket(int port)
+{
+    int listenfd;
+    struct sockaddr_in servaddr;
+    int opt = 1;
+
+    // Creating socket
+    if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    {
+        LOG(ERROR) << "Socket creation error";
+        exit(EXIT_FAILURE);
+    }
+
+    if (setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)))
+    {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+
+    bzero(&servaddr, sizeof(servaddr));
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(port);
+    int servaddr_len = sizeof(servaddr);
+
+    if (bind(listenfd, (sockaddr *)&servaddr, servaddr_len) < 0)
+    {
+        LOG(ERROR) << "Socket binding error";
+    }
+    if (listen(listenfd, 10) < 0)
+    {
+        LOG(ERROR) << "Server listening error";
+    }
+    return listenfd;
+}
+
+void cleanup(std::map<std::string, room> &database)
+{
+    for (std::map<std::string, room>::iterator iter = database.begin(); iter != database.end(); ++iter)
+    {
+        shutdown(iter->second.master_socket, SHUT_RDWR);
+        for (int i = 0; i < iter->second.slave_socket.size(); i++)
+        {
+            close(i);
+        }
+    }
+}
 
 int main(int argc, char *argv[])
 {
@@ -83,56 +150,71 @@ int main(int argc, char *argv[])
                     command += recvline[i];
                     i += 1;
                 }
-                i += 1;
-                while (recvline[i] != ' ' && i < 256 & recvline[i] != '\0')
-                {
-                    chatroom_name += recvline[i];
-                    i += 1;
-                }
                 LOG(WARNING) << "Command: " << command << "; size: " << command.length();
-                LOG(WARNING) << "Chatroom name: " << chatroom_name << "; size: " << chatroom_name.length();
                 Reply reply;
                 if (command == "CREATE")
                 {
-                    if (database.count(chatroom_name) == 0)
+                    i += 1;
+                    while (recvline[i] != ' ' && i < 256 & recvline[i] != '\0')
                     {
-                        database[chatroom_name] = (room){nextPort, -1};
+                        chatroom_name += recvline[i];
+                        i += 1;
+                    }
+                    LOG(WARNING) << "Chatroom name: " << chatroom_name << "; size: " << chatroom_name.length();
+                    int found = database.count(chatroom_name);
+
+                    if (chatroom_name.length() == 0)
+                    {
+                        reply.status = FAILURE_INVALID;
+                    }
+                    else if (found == 0)
+                    {
+                        int master_socket = new_socket(nextPort);
+                        database[chatroom_name] = (room){nextPort, master_socket, master_socket};
                         reply.status = SUCCESS;
                         nextPort += 1;
                     }
-                    else
+                    else if (found > 0)
                     {
                         reply.status = FAILURE_ALREADY_EXISTS;
                     }
                 }
-                if (command == "JOIN")
+                else if (command == "JOIN")
                 {
                 }
-                if (command == "DELETE")
+                else if (command == "DELETE")
                 {
                 }
-                if (command == "LIST")
+                else if (command == "LIST")
                 {
+                    std::string res = "";
+                    for (std::map<std::string, room>::iterator iter = database.begin(); iter != database.end(); ++iter)
+                    {
+                        std::string k = iter->first;
+                        if (std::next(iter, 1) != database.end())
+                        {
+                            res = res + k + ", ";
+                        }
+                        else
+                        {
+                            res = res + k;
+                        }
+                    }
+                    LOG(WARNING) << "List room: " << res;
+                    reply.status = SUCCESS;
+                    strcpy(reply.list_room, res.c_str());
+                    debug(database);
+                }
+                else
+                {
+                    reply.status = FAILURE_INVALID;
                 }
                 send(connfd, &reply, sizeof(reply), 0);
-                for (auto it : database)
-                {
-                    LOG(WARNING) << "Chatroom name: " << it.first << "; port: " << it.second.port;
-                    if (it.second.slave_socket.size())
-                    {
-                        LOG(WARNING) << "No socket"
-                                     << "\n";
-                    }
-                    for (auto i : it.second.slave_socket)
-                    {
-                        LOG(WARNING) << "socket: " << i;
-                    }
-                }
-                LOG(WARNING) << "\n";
             }
             close(connfd);
         }
     }
     shutdown(listenfd, SHUT_RDWR);
+    cleanup(database);
     return 0;
 }
