3002556c7326e459e0b1e3dc8a5bb7d64155ea26
Anh Nguyen <ng.huyanh.gd@gmail.com>
2023-02-27 17:49:51 -0600

    init mp-2

---
+++ MP_2/tsd.cc
@@ -0,0 +1,103 @@
+#include <ctime>
+
+#include <google/protobuf/timestamp.pb.h>
+#include <google/protobuf/duration.pb.h>
+
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <stdlib.h>
+#include <unistd.h>
+#include <google/protobuf/util/time_util.h>
+#include <grpc++/grpc++.h>
+
+#include "sns.grpc.pb.h"
+
+using google::protobuf::Timestamp;
+using google::protobuf::Duration;
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::ServerReader;
+using grpc::ServerReaderWriter;
+using grpc::ServerWriter;
+using grpc::Status;
+using csce438::Message;
+using csce438::Request;
+using csce438::Reply;
+using csce438::SNSService;
+
+class SNSServiceImpl final : public SNSService::Service {
+  
+  Status List(ServerContext* context, const Request* request, Reply* reply) override {
+    // ------------------------------------------------------------
+    // In this function, you are to write code that handles 
+    // LIST request from the user. Ensure that both the fields
+    // all_users & following_users are populated
+    // ------------------------------------------------------------
+    return Status::OK;
+  }
+
+  Status Follow(ServerContext* context, const Request* request, Reply* reply) override {
+    // ------------------------------------------------------------
+    // In this function, you are to write code that handles 
+    // request from a user to follow one of the existing
+    // users
+    // ------------------------------------------------------------
+    return Status::OK; 
+  }
+
+  Status UnFollow(ServerContext* context, const Request* request, Reply* reply) override {
+    // ------------------------------------------------------------
+    // In this function, you are to write code that handles 
+    // request from a user to unfollow one of his/her existing
+    // followers
+    // ------------------------------------------------------------
+    return Status::OK;
+  }
+  
+  Status Login(ServerContext* context, const Request* request, Reply* reply) override {
+    // ------------------------------------------------------------
+    // In this function, you are to write code that handles 
+    // a new user and verify if the username is available
+    // or already taken
+    // ------------------------------------------------------------
+    return Status::OK;
+  }
+
+  Status Timeline(ServerContext* context, ServerReaderWriter<Message, Message>* stream) override {
+    // ------------------------------------------------------------
+    // In this function, you are to write code that handles 
+    // receiving a message/post from a user, recording it in a file
+    // and then making it available on his/her follower's streams
+    // ------------------------------------------------------------
+    return Status::OK;
+  }
+
+};
+
+void RunServer(std::string port_no) {
+  // ------------------------------------------------------------
+  // In this function, you are to write code 
+  // which would start the server, make it listen on a particular
+  // port number.
+  // ------------------------------------------------------------
+}
+
+int main(int argc, char** argv) {
+  
+  std::string port = "3010";
+  int opt = 0;
+  while ((opt = getopt(argc, argv, "p:")) != -1){
+    switch(opt) {
+      case 'p':
+          port = optarg;
+          break;
+      default:
+	         std::cerr << "Invalid Command Line Argument\n";
+    }
+  }
+  RunServer(port);
+  return 0;
+}
