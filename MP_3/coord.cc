1a648607b8744d51bf5199b24c420e254e841c22
Anh Nguyen <ng.huyanh.gd@gmail.com>
2023-04-18 18:18:05 -0500

    update logging

--- MP_3/coord.cc
+++ MP_3/coord.cc
@@ -1,7 +1,82 @@
-#include <iostream>
+#include <ctime>
 
-int main()
+#include <google/protobuf/timestamp.pb.h>
+#include <google/protobuf/duration.pb.h>
+
+#include <fstream>
+#include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
+#include <stdlib.h>
+#include <unistd.h>
+#include <google/protobuf/util/time_util.h>
+#include <grpc++/grpc++.h>
+#include <glog/logging.h>
+
+#define log(severity, msg) \
+  LOG(severity) << msg;    \
+  google::FlushLogFiles(google::severity);
+
+#include "sns.grpc.pb.h"
+using csce438::ListReply;
+using csce438::Message;
+using csce438::Reply;
+using csce438::Request;
+using csce438::SNSService;
+
+#include "snsCoordinator.grpc.pb.h"
+using snsCoordinator::ClusterId;
+using snsCoordinator::FollowSyncs;
+using snsCoordinator::Heartbeat;
+using snsCoordinator::ServerInfo;
+using snsCoordinator::ServerType;
+using snsCoordinator::SNSCoordinator;
+using snsCoordinator::User;
+using snsCoordinator::Users;
+
+using google::protobuf::Duration;
+using google::protobuf::Timestamp;
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::ServerReader;
+using grpc::ServerReaderWriter;
+using grpc::ServerWriter;
+using grpc::Status;
+
+struct Cluster
 {
-    std::cout << "Hello World!";
-    return 0;
+  int port;
+  bool active;
+};
+
+std::vector<Cluster> master;
+std::vector<Cluster> slave;
+std::vector<Cluster> sync;
+
+int main(int argc, char **argv)
+{
+
+  std::string port = "9090";
+
+  int opt = 0;
+  while ((opt = getopt(argc, argv, "p:")) != -1)
+  {
+    switch (opt)
+    {
+    case 'p':
+      port = optarg;
+      break;
+    default:
+      std::cerr << "Invalid Command Line Argument\n";
+    }
+  }
+
+  std::string log_file_name = std::string("coordinator-") + port;
+  google::InitGoogleLogging(log_file_name.c_str());
+  FLAGS_log_dir = "/Users/anhnguyen/Data/CSCE438/CSCE-438/MP_3/tmp";
+  log(INFO, "Logging Initialized. Coordinator starting on port " + port);
+
+  return 0;
 }
