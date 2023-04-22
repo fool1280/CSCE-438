7cc8ce8b8bb085f0b20fd71e28efa0accaa1ee34
438 <ng.huyanh.gd@gmail.com>
2023-04-21 22:46:15 -0600

    makefile

--- MP_3/startup.sh
+++ MP_3/startup.sh
@@ -5,11 +5,11 @@ fi;
 
 ./coord -p 9090 & sleep 0.5
 
-./tsd -i localhost -c 9090 -p 3030 -d 1 -t master &
-./tsd -i localhost -c 9090 -p 3031 -d 1 -t slave 
+./tsd -i 0:0:0:0 -c 9090 -p 3030 -d 1 -t master &
+./tsd -i 0:0:0:0 -c 9090 -p 3031 -d 1 -t slave 
 
-./tsd -i localhost -c 9090 -p 3032 -d 2 -t master &
-./tsd -i localhost -c 9090 -p 3033 -d 2 -t slave 
+./tsd -i 0:0:0:0 -c 9090 -p 3032 -d 2 -t master &
+./tsd -i 0:0:0:0 -c 9090 -p 3033 -d 2 -t slave 
 
-./tsd -i localhost -c 9090 -p 3034 -d 3 -t master &
-./tsd -i localhost -c 9090 -p 3035 -d 3 -t slave 
+./tsd -i 0:0:0:0 -c 9090 -p 3034 -d 3 -t master &
+./tsd -i 0:0:0:0 -c 9090 -p 3035 -d 3 -t slave 
